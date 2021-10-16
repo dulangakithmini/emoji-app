a6de2911a0f862e3ef2025d6dae1e932496a6f05
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-10-16 13:52:13 +0530

    Refactor timeline_screen

--- lib/screens/timeline_screen.dart
+++ lib/screens/timeline_screen.dart
@@ -103,15 +103,11 @@ class TimelineScreen extends StatelessWidget {
               Expanded(
                 child: ListView(
                   children: [
-                    buildColumn(),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    buildColumn(),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    buildColumn(),
+                    PostWidget().buildPost(),
+                    SizedBox(height: 15),
+                    PostWidget().buildPost(),
+                    SizedBox(height: 15),
+                    PostWidget().buildPost(),
                   ],
                 ),
               ),
@@ -119,64 +115,6 @@ class TimelineScreen extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-
-  Container buildContainer() {
-    return Container(
-      width: 70,
-      color: Colors.orangeAccent,
-      margin: EdgeInsets.only(left: 5.0, right: 5.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.add,
-            color: Colors.blue,
-          ),
-          Text(
-            'Create story',
-            style: TextStyle(fontSize: 11),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Column buildColumn() {
-    return Column(
-      children: [
-        Container(
-          child: Row(
-            children: [
-              Container(height: 25, width: 25, color: Colors.brown),
-              SizedBox(
-                width: 5,
-              ),
-              Column(
-                children: [
-                  Text('Dulanga Kithmini'),
-                  Text(DateTime.now().toString().split('.')[0]),
-                ],
-              ),
-              Spacer(),
-              Icon(Icons.more_horiz, color: Colors.grey),
-              Icon(Icons.close, color: Colors.grey),
-            ],
-          ),
-        ),
-        SizedBox(height: 20),
-        Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text('Hi Nipuna... 😍 😘 '),
-              SizedBox(height: 20),
-              Container(height: 105, width: 75, color: Colors.blueGrey),
-            ],
-          ),
-        ),
-      ],
     );
   }
 }
--- lib/widgets/post_widget.dart
+++ lib/widgets/post_widget.dart
@@ -4,7 +4,7 @@ class PostWidget extends StatelessWidget {
   final Color colour;
 
   PostWidget({
-    @required this.colour,
+    this.colour,
   });
 
   @override
@@ -13,6 +13,43 @@ class PostWidget extends StatelessWidget {
       width: 70,
       color: colour,
       margin: EdgeInsets.only(left: 5.0, right: 5.0),
+    );
+  }
+
+  Column buildPost() {
+    return Column(
+      children: [
+        Container(
+          child: Row(
+            children: [
+              Container(height: 25, width: 25, color: Colors.brown),
+              SizedBox(
+                width: 5,
+              ),
+              Column(
+                children: [
+                  Text('Dulanga Kithmini'),
+                  Text(DateTime.now().toString().split('.')[0]),
+                ],
+              ),
+              Spacer(),
+              Icon(Icons.more_horiz, color: Colors.grey),
+              Icon(Icons.close, color: Colors.grey),
+            ],
+          ),
+        ),
+        SizedBox(height: 20),
+        Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text('Hi Nipuna... 😍 😘 '),
+              SizedBox(height: 20),
+              Container(height: 105, width: 75, color: Colors.blueGrey),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
